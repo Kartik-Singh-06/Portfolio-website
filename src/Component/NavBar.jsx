f36e44254303994bb7063823cd65b2e21150eb88
Kartik Singh <tkartik0661@gmail.com>
2025-03-06 15:42:31 +0530

    Fix issue :  resume URL.

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -4,7 +4,7 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import logo from "../assets/logo.png.png";
 import { Link } from "react-router-dom";
-import resume from "../assets/Kartik-resume-FD"
+import resume from "../assets/Kartik-resume-FD.pdf"
 import {
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
