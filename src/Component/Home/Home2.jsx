c0d5838999a54e4a8945cd998e63118069cf4bb2
Kartik Singh <tkartik0661@gmail.com>
2025-10-09 00:27:18 +0530

    Add : Picture in the home page

--- src/Component/Home/Home2.jsx
+++ src/Component/Home/Home2.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../assets/avatar.svg";
+import square from "../../assets/Square.png";
 import Tilt from "react-parallax-tilt";
 import { AiFillGithub, AiOutlineTwitter, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
@@ -35,7 +36,7 @@ function Home2() {
 
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={square} className="img-fluid photo" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
